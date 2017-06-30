1f01bce7ce403212fbcf913e9557a85c00b2a31e
narini <natringsing@gmail.com>
2017-06-30 11:45:39 +0200

    Add files via upload

---
+++ problem3.m
@@ -0,0 +1,6 @@
+%The prime factors of 13195 are 5, 7, 13 and 29.
+%What is the largest prime factor of the number 600851475143 ?
+
+%Matlab har en funktion kaldet factor der kan finde prime opdelingen
+n=600851475143;
+f = factor(n)
